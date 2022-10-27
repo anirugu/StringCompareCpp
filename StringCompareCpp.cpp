2c93dc59dd43c1799fb8f73b9d63f399ec834966
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-10-27 11:17:25 +0530

    Initial commit

---
+++ StringCompareCpp.cpp
@@ -0,0 +1,14 @@
+// StringCompareCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string one = "Jai Mata Di";
+    string two = "Jor se Bolo Jai Mata Di";
+    std::cout << "Hello World!\n";
+}
